b9bcdd9da430cc836c16c7e223a850cf5128dc9e
deviasrilestari20312125oo@gmail.com <ayulintang95@gmail.com>
2024-06-21 15:46:12 +0700

    Update 21/06/24

--- App.js
+++ App.js
@@ -1,35 +1,54 @@
 import React, { useState } from 'react'
-// Import axios, BASE_URL, dan API_KEY
 import axios from 'axios'
 import { BASE_URL, API_KEY } from './src/constant'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
+
 const App = () => {
-  // Definisikan state "weatherData" dan "setWeatherData"
-  const [weatherData, setWeatherData] = useState()
+  const [status, setStatus] = useState('')
+  const [weatherData, setWeatherData] = useState(null)
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return null
+    }
+  }
+
   const searchWeather = (location) => {
+    setStatus('loading')
+    setWeatherData(null)
     axios
       .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then((response) => {
         const data = response.data
-        // Tambahkan code di bawah
         data.visibility /= 1000
         data.visibility = data.visibility.toFixed(2)
-        data.main.temp -= 273.15 // Konversi Kelvin ke Celcius
+        data.main.temp -= 273.15
         data.main.temp = data.main.temp.toFixed(2)
         setWeatherData(data)
+        setStatus('success')
       })
       .catch((error) => {
-        console.log(error)
+        setStatus('error')
       })
   }
 
   return (
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
-      {/* Tampilkan data cuaca ketika ada weatherData */}
-      {weatherData && <WeatherInfo  weatherData={weatherData}  />}
+      <View style={styles.margintTop20}>{renderComponent()}</View>
     </View>
   )
 }
